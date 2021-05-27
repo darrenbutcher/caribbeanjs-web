2b561aaf45aa8e128ef3e2be7c5f2e81745798a7
Darren Butcher <dbutcher6@gmail.com>
2021-05-27 20:32:20 +0000

    add padding to header

--- components/organisms/header.js
+++ components/organisms/header.js
@@ -2,7 +2,7 @@ import Icon from '@/atoms/icon'
 
 function Header() {
   return (
-    <header className="h-16 flex items-center">
+    <header className="h-16 py-4 flex items-center">
       <div className="flex space-x-4 ml-auto">
         <a href="https://jamstack.org" target="_blank" className="font-poppins font-bold text-gray-500 border-b-4 border-white hover:text-purple-500 hover:border-b-4 hover:border-gray-900">Jamstack</a>
         <a href="https://2020.stateofjs.com/en-US/" target="_blank" className="font-poppins font-bold text-gray-500 border-b-4 border-white hover:text-purple-500 hover:border-b-4 hover:border-gray-900">State of JS 2020</a>
